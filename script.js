10501ccbdf04c37302703c9d849afe9ee7dcc22c
jambanix <jambanix@pm.me>
2024-09-17 21:06:27 +0100

    finish styling for all screen sizes. finished js functionality. added hover effect to radio buttons and button

--- script.js
+++ script.js
@@ -5,23 +5,25 @@ const results = document.querySelector(".results");
 const radios = form.querySelectorAll("input");
 
 const ratingSelected = () => {
-    radios.forEach((radio) => {
-        console.log(radio);
-        if (radio.checked) return radio;
-    })
+
+    for (radio of radios) {
+        if (radio.checked) return radio.labels[0].textContent
+    }
     return null;
 }
 
 btnSubmit.addEventListener("click", () => {
-    const rating = ratingSelected();
+    const rating = ratingSelected()
+    console.log(rating);
     if (rating) {
-        changeState(rating.value);
+        changeState(rating);
     }
 });
 
 function changeState(rating) {
     [form, results].forEach((item) => {
         item.classList.toggle("active");
-        results.querySelector(".score").querySelector("span").textContent = rating;
     })
+    console.log(rating);
+    results.querySelector(".score").querySelector("h4").querySelector("span").textContent = rating;
 }
